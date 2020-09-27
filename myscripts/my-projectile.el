2d16cd1bdf5cf405aec671bd043962cc3e42e2d5
Caleb Palmer <palmer.caleb@gmail.com>
2020-09-27 13:40:13 -0300

    Fixes my-projectile.el

--- myscripts/my-projectile.el
+++ myscripts/my-projectile.el
@@ -1,7 +1,9 @@
 ;;; Run init.el when switching to a projectile project.
 (defun cap/projectile-run-init ()
   (if
-      (file-exists-p (concat projectile-project-root "/proj.el"))
-      (load-file concat projectile-project-root "/proj.el"))
+      (file-exists-p (concat (projectile-ensure-project (projectile-project-root)) "proj.el"))
+      (progn
+	(load-file (concat (projectile-ensure-project (projectile-project-root)) "proj.el")))
+    )
   )
 (add-hook 'projectile-after-switch-project-hook 'cap/projectile-run-init)
